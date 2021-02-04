a1e0b8f5848d049cee628e3dba1ba62c25d35c0d
Alexis Jeanne <alexis.jeanne@campus-igs-toulouse.fr>
2021-02-04 10:57:34 +0100

    blablabla

--- src/main/java/com/microservice/articlesservice/web/controller/ArticleController.java
+++ src/main/java/com/microservice/articlesservice/web/controller/ArticleController.java
@@ -9,12 +9,15 @@ import com.microservice.articlesservice.web.exceptions.ArticleIntrouvableExcepti
 import com.microservice.articlesservice.web.exceptions.ArticlePrixEgalZeroException;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletRequest;
 import java.net.URI;
 import java.util.List;
 import java.util.Map;
@@ -24,12 +27,16 @@ import java.util.stream.Collectors;
 @RestController
 @Api(value = "API pour les opérations CRUD sur les articles")
 public class ArticleController {
+    private static final Logger logger = LoggerFactory.getLogger(ArticleController.class);
     @Autowired
     private ArticleDao articleDao;
+    @Autowired
+    private HttpServletRequest requestContext;
 
     @ApiOperation(value = "Récupère la liste des articles !")
     @GetMapping(value = "/Articles")
     public MappingJacksonValue listeArticles() {
+        logger.info("Début d'appel au service Articles pour la requête : " + requestContext.getHeader("req-id"));
         List<Article> articles = articleDao.findAll();
         SimpleBeanPropertyFilter monFiltre = SimpleBeanPropertyFilter.serializeAllExcept("prixAchat");
         FilterProvider listDeNosFiltres = new SimpleFilterProvider().addFilter("myDynamicFilter", monFiltre);
@@ -42,6 +49,7 @@ public class ArticleController {
     @ApiOperation(value = "Récupère un article grâce à son ID à condition que celui-ci soit en stock !")
     @GetMapping(value = "/Articles/{id}")
     public Article afficherUnArticle(@PathVariable int id) {
+        logger.info("Début d'appel au service Articles pour la requête : " + requestContext.getHeader("req-id"));
         return articleDao.findById(id).orElseThrow(() -> new ArticleIntrouvableException("L'article avec l'id " + id + " est INTROUVABLE"));
     }
 
@@ -49,6 +57,7 @@ public class ArticleController {
     @ApiOperation(value = "Permet de créer un article !")
     @PostMapping(value = "/Articles")
     public ResponseEntity<Void> ajouterArticle(@RequestBody Article article) {
+        logger.info("Début d'appel au service Articles pour la requête : " + requestContext.getHeader("req-id"));
         if (article.getPrix() == 0)
             throw new ArticlePrixEgalZeroException("Impossible de créer cet article, son prix est égal à 0");
         Article articleAdded = articleDao.save(article);
@@ -64,18 +73,21 @@ public class ArticleController {
     @ApiOperation(value = "Permet de supprimer un article via son ID !")
     @DeleteMapping(value = "/Articles/{id}")
     public void supprimerArticle(@PathVariable int id) {
+        logger.info("Début d'appel au service Articles pour la requête : " + requestContext.getHeader("req-id"));
         articleDao.deleteById(id);
     }
 
     @ApiOperation(value = "Permet de mettre à jour un article via son ID !")
     @PutMapping(value = "/Articles/{id}")
     public void updateArticle(@RequestBody Article article) {
+        logger.info("Début d'appel au service Articles pour la requête : " + requestContext.getHeader("req-id"));
         articleDao.save(article);
     }
 
     @ApiOperation(value = "Retourne la marge de chaque produit")
     @GetMapping(value = "/Articles/AdminArticles")
     public Map<Article, Integer> calculerMargeArticle() {
+        logger.info("Début d'appel au service Articles pour la requête : " + requestContext.getHeader("req-id"));
         return articleDao.findAll()
                 .stream()
                 .collect(Collectors.toMap(Function.identity(), article -> article.getPrix() - article.getPrixAchat()));
@@ -84,6 +96,7 @@ public class ArticleController {
     @ApiOperation(value = "Récupère la liste des articles triés par nom (asc)")
     @GetMapping(value = "/Articles/TriArticles")
     public MappingJacksonValue trierArticlesParOrdreAlphabetique() {
+        logger.info("Début d'appel au service Articles pour la requête : " + requestContext.getHeader("req-id"));
         List<Article> articles = articleDao.findAllByOrderByNom();
         SimpleBeanPropertyFilter monFiltre = SimpleBeanPropertyFilter.serializeAll();
         FilterProvider listDeNosFiltres = new SimpleFilterProvider().addFilter("myDynamicFilter", monFiltre);
